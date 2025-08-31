a309cb42f5817a73297ea930446e2811781bfc79
Davlik <vladislauskirey@yandex.by>
2025-08-31 20:48:33 +0300

    Тз номер 4 Кирей Владислав Олегович

---
+++ src/Task.java
@@ -0,0 +1,54 @@
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task() {
+        this.status = TaskStatus.NEW;
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = TaskStatus.NEW;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
